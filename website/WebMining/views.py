20da812169971ab0491ebe33df5df10499a2acd2
Misel4 <michalis_anagnostou@hotmail.com>
2019-05-18 18:14:23 +0300

    Templates are working

--- website/WebMining/views.py
+++ website/WebMining/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.core.files.storage import FileSystemStorage
+from .forms import LogForm
+from .models import Log
 from django.views.generic import TemplateView
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
@@ -19,3 +21,23 @@ def upload(request):
         context['url'] = fs.url(name)
 
     return render(request, 'WebMining/upload.html', context)
+
+
+def logfiles_list(request):
+    logs = Log.objects.all()
+    return render(request, 'WebMining/logfiles_list.html', {
+        'logs': logs
+    })
+
+
+def upload_log(request):
+    if request.method == 'POST':
+      form = LogForm(request.POST, request.FILES)
+      if form.is_valid():
+         form.save()
+      return redirect('logfiles_list')
+    else:
+     form = LogForm()
+    return render(request, 'WebMining/upload_log.html', {
+        'form': form
+    })
