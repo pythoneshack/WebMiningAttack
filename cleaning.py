1d2b279a63abc2d418f57b035fa1224e865fd7b9
ArisPoz <aris.pozidis96@gmail.com>
2019-05-18 12:37:53 +0200

    clean data add more columns

--- cleaning.py
+++ cleaning.py
@@ -2,17 +2,13 @@ import pandas as pd
 import geoip2.database
 
 fn = r'data/dataAgg'
-cols = ['Host','l','userid','Date','tz','Endpoint','Status','Data','referer','useragent']
+cols = ['Host', 'l', 'userid', 'Date', 'tz', 'Endpoint', 'Status', 'Data', 'referer', 'useragent']
 reader = geoip2.database.Reader('GeoLite2-City.mmdb')
 df = pd.read_csv(fn, delim_whitespace=True, names=cols).drop('l', 1)
-# response = reader.city(df.host)
-
 
 df['Date'] = df['Date'].map(lambda x: x.lstrip('[').rstrip(''))
-df['Date'] = df['Date'].map(lambda x: x.replace(":", " ",1))
+df['Date'] = df['Date'].map(lambda x: x.replace(":", " ", 1))
 
-# df['Date'] = df['Date'].replace("[", "")
-# df['Date'] = df['Date'].replace(":", " ", 1)
 countries = []
 latitude = []
 longitude = []
@@ -21,6 +17,12 @@ for x in df['Host']:
     countries.append(response.country.name)
     latitude.append(response.location.latitude)
     longitude.append(response.location.longitude)
+
+split = df["Endpoint"].str.split(" ", n=1, expand=True)
+df['Request'] = split[0]
+df['Endpoint'] = split[1]
+df.drop(columns=["tz"], inplace=True)
+
 df['Country'] = countries
 df['Latitude'] = latitude
 df['Longitude'] = longitude
