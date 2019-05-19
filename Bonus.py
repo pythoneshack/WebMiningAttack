f2c8078452514691566527fe1233d9475da0f404
DioStol14 <dionisis.stolis.92@gmail.com>
2019-05-19 07:37:45 +0300

    Merge branches 'General' and 'master' of https://github.com/pythoneshack/WebMiningAttack into General
    # Conflicts:
    #	BarStatus.svg
    #	SqlInjectionIndetifier.py

--- Bonus.py
+++ Bonus.py
@@ -24,8 +24,6 @@ df_most_malicious['FrequencyWebshell'] = df_attack[df_attack.Webshell == True].g
 df_most_malicious['Host'] = df_attack.groupby('Host').agg({'Host': lambda x: list(x).__getitem__(1)})
 malcious_ip_xss = df_most_malicious.sort_values(['Frequency_xss', 'Frequency_sql'], ascending=[False, False])
 sorted_malicious_ip= df_most_malicious.sort_values(['Frequency_xss', 'Frequency_sql', 'Frequency_lfi', 'FrequencyWebshell'], ascending=[False, False, False, False]).head(5)
-print(malcious_ip_xss)
-print(sorted_malicious_ip)
 
 
 line_chart = pygal.HorizontalBar()
@@ -34,39 +32,75 @@ for index, row in sorted_malicious_ip.head(5).iterrows():
     line_chart.add(row['Host'], row['Frequency_xss'] + row['Frequency_sql']+row['Frequency_lfi']+row['FrequencyWebshell'])
 line_chart.render_to_file("Top5ip.svg")
 
-# TOP 5 IP ATTACKED
-# xss=[]
-# sql=[]
-# lfi=[]
-# webshell=[]
-# df_top_attack = pd.DataFrame(columns=['Endpoint', 'Frequency_xss', 'Frequency_sql', 'Frequency_lfi', 'FrequencyWebshell','Frequency'])
-# df_top_attack['Frequency_xss'] = df_attack[df_attack.xss == True].groupby('Endpoint').size()
-# df_top_attack['Frequency_sql'] = df_attack[df_attack.sqli_injection == True].groupby('Endpoint').size()
+# #TOP 5 IP ATTACKED
+df_top_attack = pd.DataFrame(columns=['Endpoint', 'Frequency_xss', 'Frequency_sql', 'Frequency_lfi', 'FrequencyWebshell', 'Frequency'])
+#EDW
+df_attack = df_attack.applymap(lambda x: 1 if x == True else x)
+df_attack = df_attack.applymap(lambda x: 0 if x == False else x)
+df_top_attack['Endpoint'] = df_attack['Endpoint']
+# df_attack["xss"] = pd.to_numeric(df_attack.Data, errors='coerce')
+# df_attack["Webshell"] = pd.to_numeric(df_attack.Data, errors='coerce')
+# df_attack["sqli_injection"] = pd.to_numeric(df_attack.Data, errors='coerce')
+# df_attack["lfi"] = pd.to_numeric(df_attack.Data, errors='coerce')
+count = []
+for index, row in df_attack.iterrows():
+    count.append(row['xss'] + row['Webshell'] + row['sqli_injection'] + row['lfi'])
+df_top_attack['Frequency'] = count
+
+line_chart = pygal.HorizontalBar()
+line_chart.title = 'Top 5 attacked endpoint'
+df_new = pd.DataFrame({'count': df_top_attack.groupby('Endpoint').size()}).reset_index()
+for index, row in df_new.sort_values('count', ascending=False).head(5).iterrows():
+   line_chart.add(row['Endpoint'], row['count'])
+
+line_chart.render_to_file("Top5endpoints.svg")
+
+# df_top_attack['Frequency'] = df_top_attack.groupby('Endpoint')['Frequency'].count()
+# print(df_top_attack)
+
+# t_counter = 0
+# f_counter = 0
+# for x in df_top_attack['Frequency']:
+#     if x == 0:
+#         f_counter = f_counter + 1
+#     else:
+#         t_counter = t_counter + 1
+#
+# print('True :' + str(t_counter) + " False :"+ str(f_counter))
+# x = sort_values(df_top_attack['Frequency'])
+# df_top_attack['Frequency']= df_attack['xss'].size() + df_attack['sqli_injection'].size() + df_attack['lfi'].size() + df_attack['Webshell'].size()
+# df_top_attack['Endpoint'] = df_attack['Endpoint']
+# print(df_top_attack)
+
+# df_top_attack['Frequency_sql'] = df_pages[df_attack.sqli_injection == 1].groupby('Endpoint').size()
+# df_top_attack['Frequency_lfi'] = df_attack[df_attack.lfi == 1].groupby('Endpoint').size()
+# df_top_attack['FrequencyWebshell'] = df_attack[df_attack.Webshell == 1].groupby('Endpoint').size()
+#
+# #df_top_attack['Endpoint'] = df_attack.groupby('Endpoint').agg({'Endpoint': lambda x: list(x).__getitem__(1)})
+# attacked_endpoint = df_top_attack.sort_values(['Frequency_xss', 'Frequency_sql'], ascending=[False, False])
+# print(df_top_attack.head())
+
+#df_attack.to_csv("df_attack.csv")
+
+# df_top_attack['Frequency_xss'] = df_pages.groupby('Endpoint')['xss' == True].count()
+# print(df_top_attack.head())
+# df_top_attack['Frequency_sql'] = df_pages[df_attack.sqli_injection == True].groupby('Endpoint').size()
 # df_top_attack['Frequency_lfi'] = df_attack[df_attack.lfi == True].groupby('Endpoint').size()
 # df_top_attack['FrequencyWebshell'] = df_attack[df_attack.Webshell == True].groupby('Endpoint').size()
+#
 # df_top_attack['Endpoint'] = df_attack.groupby('Endpoint').agg({'Endpoint': lambda x: list(x).__getitem__(1)})
 # attacked_endpoint = df_top_attack.sort_values(['Frequency_xss', 'Frequency_sql'], ascending=[False, False])
+# print(df_top_attack.head())
 # sorted_attacked_endpoint = df_top_attack.sort_values(['Frequency_xss', 'Frequency_sql', 'Frequency_lfi', 'FrequencyWebshell'], ascending=[False, False, False, False]).head(5)
-# print(attacked_endpoint)
-# print(sorted_attacked_endpoint)
+# sort_xss_var = sort_values(xss_var)
+# print(sql_var)
+# print(lfi_var)
+# print(webshell_var)
+
 
 # line_chart = pygal.HorizontalBar()
 # line_chart.title = 'Top 5 attacked Endpoints'
-# for index, row in sorted_attacked_endpoint.head(5).iterrows():
-#     line_chart.add(row['Endpoint'], row['Frequency_xss'])
-# line_chart.render_to_file("Top5ip.svg")
-# #df_top_attack['FreqSqlInj'] = df_attack[df_attack]
-
-
-
-#
-# #malcious ip sqlInj
-# df_most_sqlInj = pd.DataFrame(columns=['Host', 'Frequency'])
-# df_most_sqlInj['Frequency'] = df_attack[df_attack.sqli_injection == True].groupby('Host').size()
-# df_most_sqlInj['Host'] = df.groupby('Host').agg({'Host': lambda x: list(x).__getitem__(1)})
-# malcious_ip_sqlInj = df_most_sqlInj.sort_values('Frequency', ascending=False)
-# print(malcious_ip_sqlInj.info())
-#
-#
-# #total_malcious = pd.DataFrame(columns=['Host', 'Frequency'])
-# #total_malcious =
+# for index, row in df_top_attack.iterrows():
+#     sum = (row['Frequency_xss'] + row['Frequency_sql']+row['Frequency_lfi']+row['FrequencyWebshell'])
+#     line_chart.add(row['Endpoint'], sum)
+# line_chart.render_to_file("Top5Endpoint.svg")
